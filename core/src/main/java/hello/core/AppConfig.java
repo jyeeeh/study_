309c074d576554b951a80bc8840021b7014e49c9
jyeeeh <universe78a@gmail.com>
2024-05-02 13:11:32 +0900

    refact : AppConfig 리팩터링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,10 +11,17 @@ import hello.core.order.OrderServiceImpl;
 
 // 전체 설정, 구성
 // 실제 동작에 필요한 구현 객체를 생성
+// 역할과, 구현클래스 한눈에 파악가능
 public class AppConfig {
 
+    // Repository 역할 표기 위해 분리작업
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository(){
+        // 다른 구현체로 변경 시 해당 부분만 변경
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
@@ -20,4 +29,10 @@ public class AppConfig {
                 new MemoryMemberRepository(),
                 new FixDiscountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        // 고정할인정책 사용
+        return new FixDiscountPolicy();
+    }
+
 }
