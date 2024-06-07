b48ecc6a3a34b7f88b703cedb3db2a0b41b8032c
jyeeeh <universe78a@gmail.com>
2024-06-07 17:57:45 +0900

    add: watch 감시자

--- vue/section2/05_native/app.js
+++ vue/section2/05_native/app.js
@@ -2,10 +2,51 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName:'',
+      fullname: ''
     };
   },
-  methods: {
+  watch: {
+    name(value){
+      if(value===''){
+        this.fullname='';
+      } else {
+        this.fullname=value+' '+this.lastName;
+      }
+    },
+    lastName(value){
+      if(value===''){
+        this.fullname='';
+      } else {
+        this.fullname=this.name+' '+value;
+      }
+    }
+  },
+  // 연산
+  // 연산 프로퍼티 이름은 데이터 프로퍼티처럼 정하기
+computed: {
+  //computed 프로퍼티에 있는 fullname HTML 코드에 사용할 수 있음
+  // fullname(){
+  //   console.log("eeee");
+  //   if (this.name===''){
+  //     return '';
+  //   } else {
+  //   return this.name + ' ' + "Choi";
+  //   }
+  // }
+  
+},
+
+methods: {
+    outputFullname(){
+      console.log('running');
+      if (this.name===''){
+        return '';
+      } else {
+      return this.name + ' ' + "Choi";
+      }
+    },
     setName(event, lastName) {
       this.name = event.target.value;
     },
