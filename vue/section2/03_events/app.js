297f07d9680bdca86032ec6fef7930f7fbe91190
jyeeeh <universe78a@gmail.com>
2024-06-13 21:15:39 +0900

    add:

--- vue/section2/03_events/app.js
+++ vue/section2/03_events/app.js
@@ -2,16 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
 
   methods: {
-    add() {
-      //counter update
-      this.counter++;
+    //이름 작성
+    //브라우저가 기본 이벤트 객체를 제공하도록 함
+    //기본 이벤트 객체가 이벤트 정보 다량
+    setName(event, lastName) {
+      this.name = event.target.value+' '+lastName;
     },
-    reduce(){
-      this.counter--;
+    add(num) {
+      //counter update
+      this.counter=this.counter+num;
+    },
+    reduce(num){
+      this.counter=this.counter-num;
     }
   }
 });
