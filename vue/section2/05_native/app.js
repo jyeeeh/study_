ae91bdf73ab63e9bfeb523e9608454cbe1362bf5
jyeeeh <universe78a@gmail.com>
2024-06-15 16:19:21 +0900

    add: 05 native

---
+++ vue/section2/05_native/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput(){
+      //초기화
+      this.name='';
+      
+    }
+  }
+});
+
+app.mount('#events');
