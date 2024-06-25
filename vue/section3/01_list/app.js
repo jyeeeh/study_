2f646b4ac4229c71556d186650f5f2e68630fab7
jyeeeh <universe78a@gmail.com>
2024-06-25 19:11:35 +0900

    add: remove list item

--- vue/section3/01_list/app.js
+++ vue/section3/01_list/app.js
@@ -11,6 +11,10 @@ const app = Vue.createApp({
       // 사용자가 입력한 내용을 새로운 목표로 push
       this.goals.push(this.enteredGoalValue);
 
+    },
+    removeGoal(idx){
+      this.goals.splice(idx, 1)
+
     }
   }
 });
