b2bdabfe50a1cecafa237a390b07da1cdd6006ae
JIHYE <145963612+jyeeeh@users.noreply.github.com>
2024-09-05 11:57:27 +0900

    add: TreeItemApp.vue

---
+++ vue/Form/TreeItemApp.vue
@@ -0,0 +1,47 @@
+<!-- 트리 뷰 -->
+
+<script setup>
+import TreeItem from '/frontend/src/components/prac/TreeItem.vue'
+import {ref} from 'vue'
+
+// 트리구조 데이터
+const treeData = ref({
+      name: 'My Tree',
+      children: [
+        {name: 'first'},
+        {name: 'second'},
+        {name: 'third'},
+        {
+          name: 'have child',
+          children: [
+            {name: 'first'},
+            {name: 'second'},
+            {name: 'third'},
+            {
+              name: 'have child',
+              children: [
+                {name: 'first'},
+                {name: 'second'}
+              ]
+            }
+          ]
+        }
+      ]
+    })
+</script>
+
+<template>
+  <ul>
+    <TreeItem class="item" :model="treeData"></TreeItem>
+  </ul>
+</template>
+
+<style>
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
